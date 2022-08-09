7bd67847d5ea1cf056b0b777fa674b600718605b
Karun <mailto.karun.py@gmail.com>
2022-08-10 00:12:12 +0530

    Minor Edit

--- Matrix_Polynomial/Matrix_Poly.cpp
+++ Matrix_Polynomial/Matrix_Poly.cpp
@@ -13,5 +13,17 @@ struct Term {
 
 struct Poly {
     int n; //Number of terms in polynomial
-    struct Ter *m;
+    struct Term *m;
 };
+
+int main() {
+    struct Poly *p;
+    std::cin>>p->n;
+    p->m = new Term[p->n];
+    std::cout<<"Enter Terms";
+
+    for(int i = 0; i < p->n; i++ ){
+        std::cin>>p->m[i].coeff;
+        std::cin>>p->m[i].exp;
+    }
+}

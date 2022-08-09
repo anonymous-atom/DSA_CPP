59708745828467c92c86a990a88bb1dec0164821
Karun <mailto.karun.py@gmail.com>
2022-08-09 23:25:13 +0530

    add function

--- Matrix_Polynomial/Sparse_matrix.cpp
+++ Matrix_Polynomial/Sparse_matrix.cpp
@@ -18,19 +18,50 @@ struct Sparse {
     struct Element *e;
 };
 
-void set(struct Sparse *e) {
+void set(struct Sparse *s) {
     std::cout<<"Enter Dimensions: ";
-    std::cin>> e->m;
-    std::cin>> e->n;
+    std::cin>> s->m;
+    std::cin>> s->n;
     std::cout<<"Enter Number Of Non Zero Elements: ";
-    std::cin>>e->num;
-    e->e = new Element[e->num];
-    for(int i = 0; i < e->num; i++){
-        std::cin>>e->e[i].r;
-        std::cin>>e->e[i].c;
-        std::cin>>e->e[i].x;
+    std::cin>>s->num;
+    s->e = new Element[s->num];
+    for(int i = 0; i < s->num; i++){
+        std::cin>>s->e[i].r;
+        std::cin>>s->e[i].c;
+        std::cin>>s->e[i].x;
     }
 }
+//Let's create a function to add matrices
+
+Sparse add(Sparse *s1, Sparse *s2){
+    if(s1->n != s2->n && s1->m != s2->m);
+     Sparse *sum;
+     sum = new Sparse;
+     sum->m = s1->m;
+     sum->n = s1->n;
+     sum->e = new Element[s1->num+s2->num];
+     int i,j,k = 0;
+     while (i < s1->num && j < s2->num ) {
+
+         if(s1->e[i].r < s2->e[j].r)
+             sum->e[k++] = s1->e[i++];
+         else if (s1->e[i].r > s2->e[j].r)
+             sum->e[k++] = s2->e[i++];
+
+         else {
+             if (s1->e[i].c < s2->e[j].c)
+                 sum->e[k++] = s1->e[i++];
+             else if (s1->e[i].c > s2->e[j].c)
+                 sum->e[k++] = s2->e[j++];
+             else {
+                 sum->e[k].r = s1->e[i].r;
+                 sum->e[k].c = s1->e[i].c;
+                 sum->e[k++].x = s1->e[i++].x + s2->e[j++].x;
+             }
+         }
+     }
+}
+
 
 int main() {
     struct Sparse e;

c9b10f67a303a775c304996a55f7985a405d321d
Karun <mailto.karun.py@gmail.com>
2022-08-25 11:13:03 +0530

    Insertion Sort Function

--- Search_Algos/Bubble_Sort_2.cpp
+++ Search_Algos/Bubble_Sort_2.cpp
@@ -5,13 +5,17 @@
 #include <iostream>
 
 void BubbleSort(int arr[], int n) {
+    int flag;
     for(int i = 0; i < n-1; i++) {
+        flag = 0;
         for (int j = 0; j < n-1-i; ++j) {
             if(arr[j] > arr[j+1]) {
                 int tmp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = tmp;
+                flag = 1;
             }
+            if(flag == 0) break;
         }
     }
 
--- Search_Algos/Insertion_Sort.cpp
+++ Search_Algos/Insertion_Sort.cpp
@@ -5,28 +5,21 @@
 #include <iostream>
 using namespace std;
 
+void InsertionSort(int arr[], int n) {
+    for(int i = 1; i < n; i++) {
+        int x  = arr[i];
+        int j = i-1;
+        while (j > -1 && x < arr[j]) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = x;
+    }
+}
 int main ()
 {
-
-    int Arr[] = { 2, 7, 8, 12, 4 };
-    int n = 5;
-
-    for (int i = 1; i <= n-1; i++)
-    {
-
-        int val = Arr[i];
-        int ptr = i;
-        while (ptr > 0 && val < Arr[ptr - 1])
-        {
-            Arr[ptr] = Arr[ptr-1];
-            ptr--;
-        }
-        Arr[ptr] = val;
-    }
-
-    for(int i: Arr)
-        cout<<i<<" ";
-
-
+    int arr[] = {5,7,8,3,2};
+    InsertionSort(arr, 5);
+    for(int i : arr) std::cout<<i<<std::endl;
     return 0;
 }

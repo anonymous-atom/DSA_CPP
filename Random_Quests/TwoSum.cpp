2033ce03ee866f1fc50183c712dd51ea268cd985
Karun <mailto.karun.py@gmail.com>
2022-08-30 23:45:09 +0530

    Added 2 Sum Algo, Tag: Easy

---
+++ Random_Quests/TwoSum.cpp
@@ -0,0 +1,20 @@
+//
+// Created by karun on 30/8/22.
+//
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    unordered_map<int, int> map;
+    vector<int> twoSum(vector<int> nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.find(nums[i]) != map.end())
+                return vector<int>{map[nums[i]], i};
+            map[target - nums[i]] = i;
+        }
+        return vector<int>{};
+    }
+};
